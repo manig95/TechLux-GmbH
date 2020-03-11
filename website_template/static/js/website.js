46202d3a58c4f5be1a527591acdb12747a1cb441
Francis Xavier S <francis.xavier@pptservices.com>
2020-03-11 20:26:13 +0530

    changes in Header Menu

--- website_template/static/js/website.js
+++ website_template/static/js/website.js
@@ -9,3 +9,14 @@ $( document ).ready(function() {
         $('#wrap').css('opacity', 'unset')
 	  });
 });
+
+//mobile menu toggle
+function openSideMenu() {
+document.getElementById("mbl-menu-open").style.display = "block";
+document.getElementById("mbl-menu-open").style.width = "88%";
+}
+
+function closeSideMenu() {
+document.getElementById("mbl-menu-open").style.width = "0%";
+document.getElementById("mbl-menu-open").style.display = "none";
+}
