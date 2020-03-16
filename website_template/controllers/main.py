af5d3efb36de5eaf9514a8b7a3eee55b7c254d79
Francis Xavier S <francis.xavier@pptservices.com>
2020-03-16 20:20:39 +0530

    menu issue and promocode

--- website_template/controllers/main.py
+++ website_template/controllers/main.py
@@ -1,20 +1,51 @@
 from odoo import http
 from odoo.http import request
-from odoo.addons.sale.controllers.variant import VariantController
+from odoo import SUPERUSER_ID
+from odoo.addons.mass_mailing.controllers.main import MassMailController
 
-# class WebsiteSaleVariantController(VariantController):
-#     @http.route(['/sale/get_combination_info_website'], type='json', auth="public", methods=['POST'], website=True)
-#     def get_combination_info_website(self, product_template_id, product_id, combination, add_qty, **kw):
-#         """Special route to use website logic in get_combination_info override.
-#         This route is called in JS by appending _website to the base route.
-#         """
-#         kw.pop('pricelist_id')
-#         res = self.get_combination_info(product_template_id, product_id, combination, add_qty, request.website.get_current_pricelist(), **kw)
-#
-#         carousel_view = request.env['ir.ui.view'].render_template('website_sale.shop_product_carousel',
-#             values={
-#                 'product': request.env['product.template'].browse(res['product_template_id']),
-#                 'product_variant': request.env['product.product'].browse(res['product_id']),
-#             })
-#         res['carousel'] = carousel_view
-#         return res
+class MassMailController(MassMailController):
+
+    @http.route('/website_mass_mailing/subscribe', type='json', website=True, auth="public")
+    def subscribe(self, list_id, email, **post):
+        ContactSubscription = request.env['mailing.contact.subscription'].sudo()
+        Contacts = request.env['mailing.contact'].sudo()
+        name = ''
+        if Contacts:
+            name, email = Contacts.get_name_email(email)
+
+        subscription = ContactSubscription.search([('list_id', '=', int(list_id)), ('contact_id.email', '=', email)], limit=1)
+        if not subscription:
+            # inline add_to_list as we've already called half of it
+            contact_id = Contacts.search([('email', '=', email)], limit=1)
+            if not contact_id:
+                contact_id = Contacts.create({'name': name, 'email': email})
+            ContactSubscription.create({'contact_id': contact_id.id, 'list_id': int(list_id)})
+        elif subscription.opt_out:
+            subscription.opt_out = False
+        # add email to session
+        request.session['mass_mailing_email'] = email
+        mass_mailing_list = request.env['mailing.list'].sudo().browse(list_id)
+        return {'toast_content': mass_mailing_list.toast_content}
+
+
+class WebsiteCoupon(http.Controller):
+
+    @http.route(['/generate_promo_code'], type='http', auth="public", csrf=False, website=True)
+    def send_category_enquiry(self, **post):
+        user = request.env['res.users'].browse(SUPERUSER_ID)
+        if post.get('name') and post.get('email_id') and post.get('comment'):
+            values = {
+                'record_name': post.get('name'),
+                'model': 'mail.message',
+                'message_type': 'email',
+                'reply_to': post.get('mail_id')
+            }
+            rec_id = request.env['mail.message'].sudo().create(values)
+            template_id = request.env.ref('website_product_category.website_enquiry_mail_template')
+            print
+            post, '========================='
+            if template_id:
+                template_id.sudo().with_context({'mail_id': post.get('email_id'), 'name': post.get('name'), 'comments': post.get('comment'), 'search_string': post.get("search_string")}).send_mail(rec_id.id, force_send=True)
+            return 'mail_sent'
+        else:
+            return 'mail_not_sent'
--- website_template/static/js/website.js
+++ website_template/static/js/website.js
@@ -8,15 +8,53 @@ $( document ).ready(function() {
         $('.modalDialog').fadeOut(600);
         $('#wrap').css('opacity', 'unset')
 	  });
+
+
+    $('#view-code-btn').click(function() {
+//        $('#coupon-code-view').css("display", "none");
+          $('#coupon-code-view').hide();
+          $('#coupon-code-view-show').show();
+    });
+
+	/*$('#view-code-btn').bind('click', function(){
+        $.ajax({
+            url: "/send_category_enquiry/",
+             type: 'POST',
+             data: {comment: $(".pt_description").val(),email_id: $("#mail_id").val(),name: $("#name").val(),search_string: $(".search-query").val()},
+             success: function(result){
+                 if (result =='mail_sent') {
+                   $('#coupon-code-view').hide();
+                   $('#coupon-code-view-show').show();
+                  } else {
+                      alert("Mail not sent, Please try after some time");
+                  }
+             }});
+    });*/
 });
 
 //mobile menu toggle
 function openSideMenu() {
 document.getElementById("mbl-menu-open").style.display = "block";
 document.getElementById("mbl-menu-open").style.width = "88%";
+$('#mb-menu-btn').attr('style','display: none !important');
+document.getElementById("mbl-menu-close").style.display = "block";
 }
 
 function closeSideMenu() {
 document.getElementById("mbl-menu-open").style.width = "0%";
 document.getElementById("mbl-menu-open").style.display = "none";
+document.getElementById("mb-menu-btn").style.display = "block";
+document.getElementById("mbl-menu-close").style.display = "none";
 }
+
+
+$( window ).resize(function() {
+var test = $( window ).width();
+if( test >= "971" ){
+$('#mb-menu-btn').attr('style','display: none !important');
+document.getElementById("mbl-menu-close").style.display = "none";
+}
+else{
+$('#mb-menu-btn').attr('style','display: block !important');
+}
+});
