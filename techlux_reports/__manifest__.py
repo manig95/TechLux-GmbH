9f69df7347e5d771e56c4adafe2eba0bdd0aced9
Francis Xavier S <francis.xavier@pptservices.com>
2020-03-25 17:14:21 +0530

    changes in report

---
+++ techlux_reports/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'TechLux Report Customization',
+    'version': '1',
+    'category': 'Sale,account',
+    'author': 'PPTS India Pvt  Ltd',
+    'summary': 'TechLux Report Customization',
+    'description': """
+                TechLux Report Customization
+                    """,
+    'depends': ['web','sale'],
+    'data': [
+             "report/report_layout.xml",
+            "report/sale_report_template.xml",
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
