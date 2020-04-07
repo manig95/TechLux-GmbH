62ef8ef46c39988187a0d23f112e32b43dc43df8
PPTS <zbenarroch>
2020-04-07 15:51:01 +0530

    added custom emails module

---
+++ techlux_emails/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'TechLux Emails Customization',
+    'version': '1',
+    'category': 'Sale,stock',
+    'author': 'PPTS India Pvt Ltd',
+    'summary': 'TechLux Emails Customization',
+    'description': """
+                TechLux Emails Customization
+                    """,
+    'depends': ['sale','stock'],
+    'data': [
+            "data/mail_data.xml"
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
