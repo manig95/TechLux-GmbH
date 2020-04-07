04b60f37db1e95c0a21c7984262ca3ef8a5fbcc1
PPTS <zbenarroch>
2020-04-07 18:32:27 +0530

    change in mail templates

--- techlux_emails/models/stock_picking.py
+++ techlux_emails/models/stock_picking.py
@@ -1,10 +1,10 @@
-from odoo import models
+# from odoo import models
 
-class Picking(models.Model):
-    _inherit = 'stock.picking'
+# class Picking(models.Model):
+#     _inherit = 'stock.picking'
 
 
-    def _send_confirmation_email(self):
-        for stock_pick in self.filtered(lambda p: p.company_id.stock_move_email_validation and p.picking_type_id.code == 'outgoing'):
-            delivery_template_id = stock_pick.company_id.stock_mail_confirmation_template_id
-            delivery_template_id.with_context().send_mail(stock_pick.id, force_send=True)
+#     def _send_confirmation_email(self):
+#         for stock_pick in self.filtered(lambda p: p.company_id.stock_move_email_validation and p.picking_type_id.code == 'outgoing'):
+#             delivery_template_id = stock_pick.company_id.stock_mail_confirmation_template_id
+#             delivery_template_id.with_context().send_mail(stock_pick.id, force_send=True)
