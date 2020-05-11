3f3b789616a3450c680b76ab14d9536d66ecc620
Francis Xavier S <francis.xavier@pptservices.com>
2020-05-11 13:45:04 +0530

    Invoice ESR Print

--- techlux_reports/models/invoice.py
+++ techlux_reports/models/invoice.py
@@ -8,3 +8,7 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     delivery_no = fields.Char("Delivery Note No.")
+
+    def print_invoice(self):
+        self.ensure_one()
+        return self.env.ref('techlux_reports.l10n_ch_isr_report_with_invoice').report_action(self)
