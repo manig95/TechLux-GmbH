caee515f6ce2b2533333d1889e8c62d255b07c4b
Francis Xavier S <francis.xavier@pptservices.com>
2020-05-14 10:48:45 +0530

    product update

--- custom_sale/models/product.py
+++ custom_sale/models/product.py
@@ -7,7 +7,7 @@ class ProductTemplate(models.Model):
     list_price = fields.Float(
         'Price excl.', default=1.0,
         digits='Product Price',
-        help="Price at which the product is sold to customers.")
+        help="Price at which the product is sold to customers.",translate=True)
 
 
 class ProductProduct(models.Model):
@@ -16,4 +16,4 @@ class ProductProduct(models.Model):
     lst_price = fields.Float(
         'Price excl.', compute='_compute_product_lst_price',
         digits='Product Price', inverse='_set_product_lst_price',
-        help="The sale price is managed from the product template. Click on the 'Configure Variants' button to set the extra attribute prices.")
+        help="The sale price is managed from the product template. Click on the 'Configure Variants' button to set the extra attribute prices.",translate=True)
